a543aabedd34fbe3971fc5e11984104b9770853e
Prowdz <nothing>
2018-08-01 13:50:58 -0400

    prepareTeams() now announces if it successfully runs

--- src/smp/edgecraft/uhc/core/managers/UHCManager.java
+++ src/smp/edgecraft/uhc/core/managers/UHCManager.java
@@ -83,6 +83,8 @@ public class UHCManager {
         {
             uhcPlayers.add(new UHCPlayer(player));
         }
+
+        announce(ChatColor.GREEN + "Successfully created teams");
     }
   
     public static void start() {

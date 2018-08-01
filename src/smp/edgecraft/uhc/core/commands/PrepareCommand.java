04cfdf813a89a7fa3222ea48c4478f0fb87332de
CJMinecraft01 <speed2pc2@gmail.com>
2018-08-01 18:32:25 +0100

    Make prepare method use /uhc prepare world and /uhc prepare teams

--- src/smp/edgecraft/uhc/core/commands/PrepareCommand.java
+++ src/smp/edgecraft/uhc/core/commands/PrepareCommand.java
@@ -9,7 +9,12 @@ public class PrepareCommand extends GameCommand {
 
     @Override
     public void onCommand(Player player, String[] args) {
-        UHCManager.prepareWorld();
-        UHCManager.prepareTeams();
+        if (args.length != 1) {
+            player.sendMessage(ChatColor.RED + "Please choose whether to prepare the teams or the world");
+            return;
+        }
+
+        if (args[0].equalsIgnoreCase("world")) UHCManager.prepareWorld();
+        else if (args[0].equalsIgnoreCase("teams")) UHCManager.prepareTeams();
     }
 }

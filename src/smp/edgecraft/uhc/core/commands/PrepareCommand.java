f5cb83493302bb8012de6545482fa52eccc7fa42
CJMinecraft01 <speed2pc2@gmail.com>
2018-08-02 15:52:26 +0100

    Please?

--- src/smp/edgecraft/uhc/core/commands/PrepareCommand.java
+++ src/smp/edgecraft/uhc/core/commands/PrepareCommand.java
@@ -1,6 +1,5 @@
 package smp.edgecraft.uhc.core.commands;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import smp.edgecraft.uhc.core.managers.UHCManager;
 
@@ -9,12 +8,6 @@ public class PrepareCommand extends GameCommand {
 
     @Override
     public void onCommand(Player player, String[] args) {
-        if (args.length != 1) {
-            player.sendMessage(ChatColor.RED + "Please choose whether to prepare the teams or the world");
-            return;
-        }
-
-        if (args[0].equalsIgnoreCase("world")) UHCManager.prepareWorld();
-        else if (args[0].equalsIgnoreCase("teams")) UHCManager.prepareTeams();
+        UHCManager.prepareTeams();
     }
 }
